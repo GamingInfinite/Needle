6826f3679e5585d6ab86d5edac107a27ebdab6d6
VoidBaroness <52724070+GamingInfinite@users.noreply.github.com>
2025-09-08 16:33:06 -0500

    TONY STARK BUILT THIS IN A CAVE
    WITH A BOX OF SCRAPS

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,216 @@
+use reqwest::{blocking::get, Client};
+use std::{
+    env::temp_dir,
+    fs::{self, File},
+    io::{copy, Write},
+    path::{Path, PathBuf},
+    process::Command,
+};
+use tokio::io::AsyncWriteExt;
+use zip::ZipArchive;
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn download_and_extract_bepinex(ss_path: String) -> Result<String, String> {
+    let win_url = "https://github.com/BepInEx/BepInEx/releases/download/v5.4.23.3/BepInEx_win_x64_5.4.23.3.zip";
+
+    let mut temp_path: PathBuf = temp_dir();
+    temp_path.push("bepinex.zip");
+
+    let res = get(win_url).map_err(|e| format!("Failed to fetch file: {}", e))?;
+    let bytes = res
+        .bytes()
+        .map_err(|e| format!("Failed to read bytes: {}", e))?;
+
+    {
+        let mut file =
+            File::create(&temp_path).map_err(|e| format!("Failed to create temp file: {}", e))?;
+        file.write_all(&bytes)
+            .map_err(|e| format!("Failed to write zip: {}", e))?;
+    }
+
+    let ss_buf = PathBuf::from(ss_path);
+    let file = File::open(&temp_path).map_err(|e| format!("Failed to open temp zip: {}", e))?;
+    let mut archive =
+        ZipArchive::new(file).map_err(|e| format!("Failed to read zip archive: {}", e))?;
+
+    for i in 0..archive.len() {
+        let mut file = archive
+            .by_index(i)
+            .map_err(|e| format!("Failed to access file in archive: {}", e))?;
+        let outpath = ss_buf.join(file.mangled_name());
+
+        if file.name().ends_with("/") {
+            fs::create_dir_all(&outpath)
+                .map_err(|e| format!("Failed to create dir in extract: {}", e))?;
+        } else {
+            if let Some(parent) = outpath.parent() {
+                if !parent.exists() {
+                    fs::create_dir_all(parent)
+                        .map_err(|e| format!("Failed to create parent dir: {}", e))?;
+                }
+            }
+            let mut outfile = File::create(&outpath)
+                .map_err(|e| format!("Failed to create extracted file: {}", e))?;
+            copy(&mut file, &mut outfile)
+                .map_err(|e| format!("Failed to copy file contents: {}", e))?;
+        }
+    }
+
+    Ok("Downloaded and Extracted Bepinex".to_string())
+}
+
+#[tauri::command]
+fn delete_bepinex_files(ss_path: String) -> Result<String, String> {
+    let base = Path::new(&ss_path);
+
+    // List of targets (true = directory, false = file)
+    let targets: [(&str, bool); 6] = [
+        ("BepInEx", true),
+        (".doorstop_version", false),
+        ("changelog.txt", false),
+        ("doorstop_config.ini", false),
+        ("winhttp.dll", false),
+        ("winhttp.disabled", false),
+    ];
+
+    for (name, is_dir) in targets {
+        let target_path = base.join(name);
+        if target_path.exists() {
+            if is_dir {
+                fs::remove_dir_all(&target_path)
+                    .map_err(|e| format!("Failed to remove directory {:?}: {}", target_path, e))?;
+            } else {
+                fs::remove_file(&target_path)
+                    .map_err(|e| format!("Failed to remove file {:?}: {}", target_path, e))?;
+            }
+        }
+    }
+
+    Ok(format!("Cleaned modding files from {}", ss_path))
+}
+
+#[tauri::command]
+fn extract_zip(zip_path: String, dest_path: String) -> Result<(), String> {
+    let zip_file = File::open(&zip_path).map_err(|e| e.to_string())?;
+    let mut archive = ZipArchive::new(zip_file).map_err(|e| e.to_string())?;
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i).map_err(|e| e.to_string())?;
+        let outpath = Path::new(&dest_path).join(file.mangled_name());
+
+        if file.name().ends_with('/') {
+            // Directory
+            fs::create_dir_all(&outpath).map_err(|e| e.to_string())?;
+        } else {
+            // File
+            if let Some(p) = outpath.parent() {
+                if !p.exists() {
+                    fs::create_dir_all(p).map_err(|e| e.to_string())?;
+                }
+            }
+            let mut outfile = File::create(&outpath).map_err(|e| e.to_string())?;
+            copy(&mut file, &mut outfile).map_err(|e| e.to_string())?;
+        }
+    }
+
+    delete_path(&zip_path).map_err(|e| e.to_string())?;
+
+    Ok(())
+}
+
+#[tauri::command]
+async fn download_command(url: String, save_path: String) -> Result<(), String> {
+    download_file(&url, &save_path)
+        .await
+        .map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+async fn delete_mod(path: String) -> Result<(), String> {
+    delete_path(&path).map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+fn open_game(path: String) {
+    let exe_path = Path::new(&path);
+
+    if !exe_path.exists() {
+        return;
+    }
+
+    let _ = Command::new(exe_path).spawn();
+}
+
+#[tauri::command]
+fn toggle_bepin(path: String) -> Result<String, String>{
+    let dll_path = Path::new(&path);
+    let disabled_path = dll_path.with_extension("disabled");
+
+    let (current, new_path) = if dll_path.exists() {
+        // If .dll exists → disable it
+        (dll_path, disabled_path)
+    } else if disabled_path.exists() {
+        // If .disabled exists → re-enable it
+        (disabled_path.as_path(), dll_path.with_extension("dll"))
+    } else {
+        return Err("Neither .dll nor .disabled file exists".into());
+    };
+
+    fs::rename(current, &new_path)
+        .map_err(|e| format!("Rename failed: {}", e))?;
+
+    Ok(new_path.to_string_lossy().to_string())
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![
+            download_and_extract_bepinex,
+            delete_bepinex_files,
+            download_command,
+            delete_mod,
+            extract_zip,
+            open_game,
+            toggle_bepin
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+pub async fn download_file(url: &str, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    // Send request
+    let response = Client::new().get(url).send().await?.error_for_status()?; // fail if not 200 OK
+
+    // Get body bytes
+    let bytes = response.bytes().await?;
+
+    // Ensure parent directory exists
+    if let Some(parent) = Path::new(path).parent() {
+        tokio::fs::create_dir_all(parent).await?;
+    }
+
+    // Write to file
+    let mut file = tokio::fs::File::create(path).await?;
+    file.write_all(&bytes).await?;
+
+    Ok(())
+}
+
+pub fn delete_path(path: &str) -> Result<(), String> {
+    let path = Path::new(path);
+
+    if path.exists() {
+        if path.is_dir() {
+            fs::remove_dir_all(path).map_err(|e| e.to_string())?;
+        } else {
+            fs::remove_file(path).map_err(|e| e.to_string())?;
+        }
+    }
+
+    Ok(())
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,261 @@
+<script lang="ts">
+    import { appConfigDir, join } from "@tauri-apps/api/path";
+    import {
+        exists,
+        mkdir,
+        writeTextFile,
+        readTextFile,
+    } from "@tauri-apps/plugin-fs";
+    import { invoke } from "@tauri-apps/api/core";
+    import { open } from "@tauri-apps/plugin-dialog";
+    import { onMount } from "svelte";
+    import DiGithubBadge from "svelte-icons/di/DiGithubBadge.svelte";
+    import FaDownload from "svelte-icons/fa/FaDownload.svelte";
+    import MdDeleteForever from "svelte-icons/md/MdDeleteForever.svelte";
+    import FaFolder from "svelte-icons/fa/FaFolder.svelte";
+    import FaPlayCircle from "svelte-icons/fa/FaPlayCircle.svelte";
+    import FaRegPlayCircle from "svelte-icons/fa/FaRegPlayCircle.svelte";
+
+    import modData from "$lib/modlinks.json";
+
+    const defaultConfig = {
+        ss_path: "",
+    };
+
+    let configDir: string;
+    let configFile: string;
+
+    let config = $state(defaultConfig);
+    let ssbp = $derived(
+        config.ss_path.replaceAll("Hollow Knight Silksong.exe", ""),
+    );
+    let ssPlugins = $derived(`${ssbp}BepInEx\\plugins`);
+
+    let doesBepInExExist = $derived(doesBepinAlrExist(config));
+
+    async function configExists() {
+        const dirExists = await exists(configDir);
+
+        const fileExists = await exists(configFile);
+
+        return dirExists && fileExists;
+    }
+
+    async function createDefaults() {
+        if (!(await configExists())) {
+            await mkdir(configDir, { recursive: true });
+            await writeTextFile(configFile, JSON.stringify(defaultConfig), {
+                create: true,
+            });
+        }
+    }
+
+    async function selectSilksong() {
+        await createDefaults();
+
+        const exePath = await open({
+            multiple: false,
+            filters: [
+                {
+                    name: "Hollow Knight Silksong.exe",
+                    extensions: ["exe"],
+                },
+            ],
+            defaultPath:
+                "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Hollow Knight Silksong",
+        });
+
+        if (!exePath) {
+            throw new Error("No executable selected");
+        }
+
+        config.ss_path = exePath;
+
+        await writeTextFile(configFile, JSON.stringify(config));
+    }
+
+    async function invokeDownloadBepinEx() {
+        await invoke("download_and_extract_bepinex", {
+            ssPath: ssbp,
+        });
+    }
+
+    async function invokeDeleteBepinEx() {
+        await invoke("delete_bepinex_files", { ssPath: ssbp });
+    }
+
+    async function doesBepinAlrExist(config: any) {
+        let bepinFolder = await join(ssbp, "BepInEx");
+        let doorstop = await join(ssbp, "winhttp.dll");
+        let disableddoorstop = await join(ssbp, "winhttp.disabled");
+
+        let bepinFolderExists = await exists(bepinFolder);
+        let doorstopExists =
+            (await exists(doorstop)) || (await exists(disableddoorstop));
+
+        let isBepinInstalled = bepinFolderExists && doorstopExists;
+        return isBepinInstalled;
+    }
+
+    async function downloadMod(url: string, fileName: string) {
+        let ssPluginPath = `${ssPlugins}\\${fileName}`;
+        await invoke("download_command", { url, savePath: ssPluginPath });
+    }
+
+    async function extract_mod(path: string, extractTo: string) {
+        await invoke("extract_zip", { zipPath: path, destPath: extractTo });
+    }
+
+    async function deleteMod(deletePath: string) {
+        console.log(deletePath);
+        await invoke("delete_mod", { path: deletePath });
+    }
+
+    async function modFileExists(mod: any): Promise<boolean> {
+        if (mod.toDelete) {
+            return await exists(`${ssPlugins}\\${mod.toDelete}`);
+        } else {
+            return await exists(`${ssPlugins}\\${mod.file}`);
+        }
+    }
+
+    async function launchModdedGame() {
+        if (!(await exists(`${ssbp}winhttp.dll`))) {
+            await invoke("toggle_bepin", { path: `${ssbp}winhttp.dll` });
+        }
+        invoke("open_game", { path: config.ss_path });
+    }
+
+    async function launchVanillaGame() {
+        if (!(await exists(`${ssbp}winhttp.disabled`))) {
+            await invoke("toggle_bepin", { path: `${ssbp}winhttp.dll` });
+        }
+        await invoke("open_game", { path: config.ss_path });
+    }
+
+    onMount(async () => {
+        configDir = await appConfigDir();
+        configFile = await join(configDir, "config.json");
+
+        await createDefaults();
+
+        config = JSON.parse(await readTextFile(configFile));
+    });
+</script>
+
+<div class="flex flex-row m-4">
+    <div class="flex flex-col w-3/8">
+        <div class="flex flex-row">
+            <button
+                class="btn btn-ghost w-full justify-start"
+                onclick={launchModdedGame}
+            >
+                <div class="w-7 h-7"><FaPlayCircle></FaPlayCircle></div>
+                Launch Modded
+            </button>
+        </div>
+        <div class="flex flex-row">
+            <button
+                class="btn btn-ghost w-full justify-start"
+                onclick={launchVanillaGame}
+            >
+                <div class="w-7 h-7"><FaRegPlayCircle></FaRegPlayCircle></div>
+                Launch Vanilla
+            </button>
+        </div>
+        <div class="flex flex-row">
+            {#await doesBepInExExist then isBepinReal}
+                {#if isBepinReal}
+                    <button
+                        class="btn btn-ghost w-full justify-start"
+                        disabled={config.ss_path == "" && isBepinReal}
+                        onclick={async () => {
+                            await invokeDeleteBepinEx();
+                        }}
+                    >
+                        <div class="w-8 h-8">
+                            <MdDeleteForever></MdDeleteForever>
+                        </div>
+                        Un-Install BepinEx
+                    </button>
+                {:else}
+                    <button
+                        class="btn btn-ghost w-full justify-start"
+                        disabled={config.ss_path == "" && isBepinReal}
+                        onclick={async () => {
+                            await invokeDownloadBepinEx();
+                        }}
+                    >
+                        <div class="w-8 h-8"><FaDownload></FaDownload></div>
+                        Install BepinEx
+                    </button>
+                {/if}
+            {/await}
+        </div>
+        <div class="flex flex-row">
+            <button
+                class="btn btn-ghost w-full justify-start"
+                onclick={selectSilksong}
+            >
+                <div class="w-8 h-8"><FaFolder></FaFolder></div>
+                Select Silksong Install
+            </button>
+        </div>
+    </div>
+    <div class="divider divider-horizontal"></div>
+    <div class="flex flex-col gap-2 w-full">
+        {#each modData as mod}
+            <div class="flex flex-row gap-10 items-center justify-between">
+                <div class="flex flex-row gap-2 items-center">
+                    <div class="font-bold text-lg">
+                        {mod.name}
+                    </div>
+                    <div>
+                        {#if !mod.version.toLowerCase().includes("v")}
+                            v
+                        {/if}{mod.version}
+                    </div>
+                    <div>
+                        by {mod.author}
+                    </div>
+                    <a
+                        class="btn btn-circle w-10 h-10"
+                        href={`https://github.com/${mod.author}/${mod.name}`}
+                        ><DiGithubBadge></DiGithubBadge></a
+                    >
+                </div>
+                <div>
+                    {#await modFileExists(mod) then exists}
+                        <input
+                            type="checkbox"
+                            checked={exists}
+                            class="toggle"
+                            onchange={async (e) => {
+                                if (e.currentTarget.checked) {
+                                    // Downloading
+                                    let download_link = `https://github.com/${mod.author}/${mod.name}/releases/download/${mod.version}/${mod.file}`;
+                                    await downloadMod(download_link, mod.file);
+                                    if (mod.file.includes(".zip")) {
+                                        await extract_mod(
+                                            `${ssPlugins}\\${mod.file}`,
+                                            `${ssbp}\\${mod.extractTo}`,
+                                        );
+                                    }
+                                } else {
+                                    // Delete Mod... wish there was a better way of disabling stuff.
+                                    let deletePath = `${ssPlugins}\\`;
+                                    if (mod.toDelete) {
+                                        deletePath += mod.toDelete;
+                                    } else {
+                                        deletePath += mod.file;
+                                    }
+                                    await deleteMod(deletePath);
+                                }
+                            }}
+                        />
+                    {/await}
+                </div>
+            </div>
+        {/each}
+    </div>
+</div>
